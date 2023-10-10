2ed2f3adf0140a1369158e04337ebee2614c5a2b
qrismus <krbashkirtseva@gmail.com>
2023-10-10 21:20:23 +0300

    v1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/qrismus/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(card, deleteFunction) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+    cardTitle.textContent = card.name;
+    deleteButton.addEventListener('click', deleteFunction);
+    cardContainer.append(cardElement);
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const cardDelete = evt.target.closest('.card');
+    cardDelete.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+    addCard(element, deleteCard)
+});
